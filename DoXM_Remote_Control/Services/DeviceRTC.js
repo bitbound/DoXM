0061d3d121c8abe837e42980050b2b7c19ca19af
Jared Goodwin <translucency@outlook.com>
2018-11-14 19:13:23 -0800

    Cleanup old files.

--- DoXM_Remote_Control/Services/DeviceRTC.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Electron = require("electron");
-var doxm = Electron.remote.getGlobal("DoXM");
-async function Init(requesterID) {
-}
-exports.Init = Init;
-//# sourceMappingURL=DeviceRTC.js.map
--- DoXM_Remote_Control/Services/Viewers.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=Viewers.js.map
