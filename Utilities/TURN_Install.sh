8e36879546d1e61c723991d127804f476ec2bf35
Jared Goodwin <translucency@outlook.com>
2019-01-18 00:28:47 -0800

    TURN script update.

--- Utilities/TURN_Install.sh
+++ Utilities/TURN_Install.sh
@@ -1,15 +1,14 @@
 # Arguments are username, password, realm.
-sudo apt-get update 
-sudo apt-get upgrade 
-sudo apt-get install -y coturn
-sudo rm -rf /var/lib/apt/lists/*
+apt-get update 
+apt-get upgrade 
+apt-get install -y coturn
 
-echo "TURNSERVER_ENABLED=1" | sudo tee /etc/default/coturn
+echo "TURNSERVER_ENABLED=1" | tee /etc/default/coturn
 
 echo "listening-port=3478
 realm=$3
 server-name=$3
 lt-cred-mech
-" | sudo tee /etc/turnserver.conf
+" | tee /etc/turnserver.conf
  
-sudo turnadmin -a -u $1 -p $2 -r $3
+turnadmin -a -u $1 -p $2 -r $3
